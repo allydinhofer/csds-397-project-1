d12d1532e1f1fe010cd6d0b50aef3f5351b24511
Ally Dinhofer <ally.dinhofer@gmail.com>
2021-03-21 19:45:14 -0400

    added action page file
    Signed-off-by: Ally Dinhofer <ally.dinhofer@gmail.com>

--- proj1.php
+++ proj1.php
@@ -42,29 +42,8 @@
 
   <!-- creating the containers for the results -->
     <article class="All Stores">
-
-        <!-- amazon section -->
-        <article class="store"> <?php
-        /* Enter the Amazon Product ISIN */
-        $amazonISIN = "B00OTWNSMM";
-
-        /* Grab the content of the HTML web page */
-        $html = file_get_contents("http://www.amazon.com/gp/aw/d/$amazonISIN");
-
-        /* Clean-up */
-        $html = str_replace("&nbsp;", "", $html);
-
-        /* The magical regex for extracting the price */
-        $regex = '/\(Prezzo|Precio|Price|Prix Amazon|Preis):?\<\/b\>([^\<]+)/i';
-
-        /* Return the price */
-
-        if (preg_match($regex, $html, $price)) {
-            $price = number_format((float)($price[2]/100), 2, '.', '');
-            echo "The price for amazon.com/dp/$amazonISIN is $price";
-        } else {
-            echo "Sorry, the item is out-of-stock on Amazon";
-        }
+        <?php
+        echo "this is amazon";
         ?> </article>
 
 
