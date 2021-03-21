e8ac6f45cea3253625806711c6704977219f47a6
Ally Dinhofer <ally.dinhofer@gmail.com>
2021-03-21 19:41:43 -0400

    got the server working
    Signed-off-by: Ally Dinhofer <ally.dinhofer@gmail.com>

--- index.php
+++ index.php
@@ -1,9 +1,63 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<title>How to put PHP in HTML - Simple Example</title>
-</head>
+     <style>
+    .all-stores {
+          margin: 0;
+          padding: 5px;
+          background-color: lightgray;
+        }
+        
+        .all-stores > h1, .store {
+          margin: 10px;
+          padding: 5px;
+        }
+        
+        .store {
+          background: white;
+        }
+        
+        .store > h2, p {
+          margin: 4px;
+          font-size: 90%;
+        }
+        </style>
+        </head>
 <body>
-<h1><?php echo "This message is from server side." ?></h1>
+
+<!-- setting up the page -->
+<body style="background-color:lightgray;"></body>
+<h1 style="color: darkblue;"> Price Comparer</h1>
+<h2 style="font-size: large;"> Easily compare prices between Amazon, Target, and Walmart. </h2>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<!-- Creates the search button -->
+<form class="example" action="action_page.php">
+    <input type="text" placeholder="Search for your item here" name="search">
+    <button type="submit"><i class="fa fa-search"></i></button>
+</form>
+
+  <!-- creating the containers for the results -->
+    <article class="All Stores">
+
+        <!-- amazon section -->
+        <article class="store"> <?php
+        echo "this is amazon";
+        ?> </article>
+
+
+        <!-- target section -->
+        <article class="store"> <?php
+        echo "this is target";
+        ?> </article> 
+
+        <!-- walmart section -->
+        <article class="store"> <?php
+        echo "this is walmart";
+        ?> </article>
+      
+  </article>
+
 </body>
 </html>
