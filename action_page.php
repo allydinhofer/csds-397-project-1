475d2fbb940faa64caf0f4ddce3bf0362aa7ac76
Ally Dinhofer <ally.dinhofer@gmail.com>
2021-03-23 09:39:57 -0400

    trying to fix amazon;
    Signed-off-by: Ally Dinhofer <ally.dinhofer@gmail.com>

--- action_page.php
+++ action_page.php
@@ -30,8 +30,32 @@
         <article class="All Stores">
             <!-- amazon section -->
             <article class="store"> <?php
-                // Grab the content of the HTML web page
                 $amazon = "https://www.amazon.com/s?k=$product_name";
+                $doc = new DOMDocument();
+                libxml_use_internal_errors(true);
+                $dom->loadHTML($amazon);
+                $result = array();
+                $a = $dom->getElementsByTagName('a')->item(0);
+                if ($a->hasAttributes()) {
+                    foreach ($a->attributes as $attr) {
+                        $name = $attr->nodeName;
+                        $value = $attr->nodeValue;    
+                        $result[$name] = $value;
+                    }
+                }
+                $first_result_amazon = file_get_contents($result[0]);
+                $first_result_amazon = str_replace("&nbsp;", "", $first_result_amazon);
+                $regex = '/\(Prezzo|Precio|Price|Prix Amazon|Preis):?\<\/b\>([^\<]+)/i';
+                /* Return the price */
+
+                if (preg_match($regex, $first_result_amazon, $price)) {
+                    $price = number_format((float)($price[2]/100), 2, '.', '');
+                    echo "$product_name is $price";
+                } else {
+                    echo "Sorry, the item is out-of-stock on Amazon";
+                }
+
+
             ?> 
             <a href=<?php echo $amazon?>>Amazon result for <?php echo $product_name ?> </a>
             </article>
