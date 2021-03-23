8daf61b7d6eae508be74c0af7af1cdb89248e215
Ally Dinhofer <ally.dinhofer@gmail.com>
2021-03-23 15:16:51 -0400

    testing
    Signed-off-by: Ally Dinhofer <ally.dinhofer@gmail.com>

--- action_page.php
+++ action_page.php
@@ -33,26 +33,37 @@
                 $amazon = file_get_contents("https://www.amazon.com/s?k=$product_name");
                 for ($i=0; $i < strlen($amazon); $i++) {
                     if ($amazon[$i-1] == ' ' && $amazon[$i] == 'P' && $amazon[$i+1] == 'r' && $amazon[$i+2] == 'i' && $amazon[$i+3] == 'c' && $amazon[$i+4] == 'e' && $amazon[$i+5] == ' ')  {
-                        echo "The price on amazon is " . $amazon[$i+6] . $amazon[$i+7] . $amazon[$i+8] . $amazon[$i+9] . $amazon[$i+10];
+                        echo "The price on Amazon is " . $amazon[$i+6] . $amazon[$i+7] . $amazon[$i+8] . $amazon[$i+9] . $amazon[$i+10];
                         break;
                     }
                 }
-
-
             ?> 
             </article>
             <!-- target section -->
             <article class="store"> <?php
-                $target = "https://www.target.com/s?searchTerm=$product_name";
+                $target = file_get_contents("https://www.target.com/s?searchTerm=$product_name");
+                for ($i=0; $i < strlen($target); $i++) {
+                    if ($target[$i] == '$') { //} && $target[$i] == 'P' && $target[$i+1] == 'r' && $target[$i+2] == 'i' && $target[$i+3] == 'c' && $target[$i+4] == 'e' && $target[$i+5] == ' ')  {
+                       // echo "The price on Target is " . $target[$i+2] . $target[$i+3] . $target[$i+4] . $target[$i+5] . $target[$i+6];
+                       echo found;
+                        break;
+                    }
+                }
+               // echo "The item is out of stock on Target";
             ?>
-            <a href=<?php echo $target?>>Target result for <?php echo $product_name ?> </a>    
             </article> 
 
             <!-- walmart section -->
             <article class="store"> <?php
-                $walmart = "https://www.walmart.com/search/?query=$product_name";
+                $walmart = file_get_contents("https://www.walmart.com/search/?query=$product_name");
+                for ($i=0; $i < strlen($walmart); $i++) {
+                    if ($walmart[$i] == '$') { //  && $walmart[$i+1] == 'r' && $walmart[$i+2] == 'i' && $walmart[$i+2] == 'c' && $walmart[$i+2] == 'e') {
+                        echo "found";
+                      //  echo "The price on Walmart is " . $walmart[$i] . $walmart[$i+1] . $walmart[$i+2] . $walmart[$i+3] . $walmart[$i+4];
+                    //    break;
+                    }
+                }
             ?>
-            <a href=<?php echo $walmart?>>Walmart result for <?php echo $product_name ?> </a> 
             </article>
       
         </article>
